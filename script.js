d2bc2cba5f933121e7c196df42e287f853e4eb16
sladumglas <sladumglas@gmail.com>
2026-01-08 20:57:07 +0900

    css

---
+++ script.js
@@ -0,0 +1,12 @@
+(() => {
+    const section = document.querySelector(".brands");
+    const list = document.getElementById("brandsList");
+    const slider = document.getElementById("brandsSlider");
+    const pagination = document.getElementById("brandsPagination");
+    const toggleBtn = document.getElementById("brandsToggle");
+    const toggleText = toggleBtn.querySelector(".brands__toggle-text");
+    const mobileQuery = window.matchMedia("(max-width: 767px)");
+    let swiper = null;
+
+    
+})();
